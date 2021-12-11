c2f5b234399334b5f4ef8f419ab692d07b48a263
James Harris <contact@jamesharris.id.au>
2021-12-11 11:55:07 +1000

    Testing making a private repo public.

--- repos.tf
+++ repos.tf
@@ -2,6 +2,7 @@ module "repo_test" {
   source      = "./modules/repo"
   name        = "test-terraform-creation"
   description = "Test creating repositories with terraform."
+  public      = true
 
   copyright_start_year = 2020
   copyright_holders    = ["Anonymous"]
