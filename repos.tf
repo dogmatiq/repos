fa4cda09a3992ad4c2e234476fbf901e9ff81b0a
James Harris <contact@jamesharris.id.au>
2021-12-11 11:59:40 +1000

    Test if removing repo causes it to be archived.

--- repos.tf
+++ repos.tf
@@ -1,9 +1,9 @@
-module "repo_test" {
-  source      = "./modules/repo"
-  name        = "test-terraform-creation"
-  description = "Test creating repositories with terraform."
-  public      = true
+# module "repo_test" {
+#   source      = "./modules/repo"
+#   name        = "test-terraform-creation"
+#   description = "Test creating repositories with terraform."
+#   public      = true
 
-  copyright_start_year = 2020
-  copyright_holders    = ["Anonymous"]
-}
+#   copyright_start_year = 2020
+#   copyright_holders    = ["Anonymous"]
+# }
