d57986739645876d15400dfac53de7bafaca50b8
James Harris <contact@jamesharris.id.au>
2021-12-14 13:33:50 +1000

    Add dogmatiq/dogma repo.

--- repos.tf
+++ repos.tf
@@ -1,3 +1,14 @@
+module "repo_dogma" {
+  source      = "./modules/repo"
+  name        = "dogma"
+  description = "A specification and API for building message-based applications in Go."
+  public      = true
+  template    = null
+
+  copyright_start_year = 2018
+  copyright_holders    = ["Kevin Millar", "Danil Petrov"]
+}
+
 module "repo_cloud" {
   source      = "./modules/repo"
   name        = "cloud"
