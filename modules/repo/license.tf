9e589d6937a0def45351a878868c708f6927221a
James Harris <contact@jamesharris.id.au>
2021-12-17 15:52:09 +1000

    Split repo/license into separate Terraform files.

--- modules/repo/license.tf
+++ modules/repo/license.tf
@@ -5,39 +5,13 @@ locals {
   )
 }
 
-resource "github_repository" "this" {
-  archive_on_destroy = true
-
-  name        = var.name
-  description = var.description
-  visibility  = var.private ? "private" : "public"
-
-  has_issues   = true
-  has_projects = false
-  has_wiki     = false
-
-  delete_branch_on_merge = true
-  allow_merge_commit     = true
-  allow_squash_merge     = false
-  allow_rebase_merge     = false
-
-  vulnerability_alerts = true
-
-  dynamic "template" {
-    for_each = var.omit_template ? [] : [null]
-    content {
-      owner      = "dogmatiq"
-      repository = "template-${var.language}"
-    }
-  }
-}
-
 resource "github_repository_file" "license" {
   repository          = github_repository.this.name
   branch              = github_repository.this.default_branch
   file                = "LICENSE"
   commit_message      = "Update license based on Terraform configuration"
   overwrite_on_create = true
+
   content = templatefile(
     "${path.module}/../../templates/LICENSE.tftpl",
     {
---
+++ modules/repo/repo.tf
@@ -0,0 +1,26 @@
+resource "github_repository" "this" {
+  archive_on_destroy = true
+
+  name        = var.name
+  description = var.description
+  visibility  = var.private ? "private" : "public"
+
+  has_issues   = true
+  has_projects = false
+  has_wiki     = false
+
+  delete_branch_on_merge = true
+  allow_merge_commit     = true
+  allow_squash_merge     = false
+  allow_rebase_merge     = false
+
+  vulnerability_alerts = true
+
+  dynamic "template" {
+    for_each = var.omit_template ? [] : [null]
+    content {
+      owner      = "dogmatiq"
+      repository = "template-${var.language}"
+    }
+  }
+}
