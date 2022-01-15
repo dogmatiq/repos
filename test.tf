525d52e67d77147d6fbd2c9939f66965ea438fc9
James Harris <contact@jamesharris.id.au>
2022-01-15 13:55:04 +1000

    Remove test repo.

--- test.tf
+++ test.tf
@@ -1,9 +1,10 @@
-resource "github_repository" "test2" {
-  name = "test-default-branch-creation"
+resource "github_repository" "test3" {
+  name      = "test-default-branch-creation"
+  auto_init = true
 }
 
 
-resource "github_branch_default" "test2" {
-  repository = github_repository.test2.name
+resource "github_branch_default" "test3" {
+  repository = github_repository.test3.name
   branch     = "main"
 }
